7886c1e59fc10390348858bc5f9585e5744e3392
Lucas Alamino <alaminolucas@gmail.com>
2024-05-10 16:24:31 -0300

    Começo da aula

---
+++ Projeto-Java/src/MinhaClasse.java
@@ -0,0 +1,21 @@
+public class MinhaClasse {
+    
+    public static void main(String[] args) {
+
+        double Altura = 1.72;
+
+        final String BR = "brasil";
+        // Quando tem o "final", a variável não pode ser alterada
+
+        String primeiroNome = "Lucas";
+        String segundoNome = "Alamino";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
