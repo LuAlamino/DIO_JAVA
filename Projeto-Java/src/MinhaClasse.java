36a80431cd4abb042201498affbff001f3d14399
Lucas Alamino <alaminolucas@gmail.com>
2024-05-13 13:44:54 -0300

    Operadores parte 1

--- Projeto-Java/src/MinhaClasse.java
+++ Projeto-Java/src/MinhaClasse.java
@@ -13,9 +13,23 @@ public class MinhaClasse {
         String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
 
         System.out.println(nomeCompleto);
+
+        
+
     }
 
     public static String nomeCompleto(String primeiroNome, String segundoNome) {
         return "resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+
+       
+
+
     }
+
+    
+
+
+
+
+
 }
